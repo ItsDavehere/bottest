668814e93033d28ef354d5f6935290d134202fc7
ItsDavehere <david12ali9@gmail.com>
2018-03-01 02:13:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ bot.registry.registerDefaults();
 
 bot.registry.registerCommandsIn(__dirname + "/");
 
-var bot_token= "NDE4NTI2MzI3ODE4NDIwMjI0.DXi2jA.hdhpw7XDJAGDu7NBC5Q3KKgNJ5E"
+
 bot.login(process.env.bot_token);
